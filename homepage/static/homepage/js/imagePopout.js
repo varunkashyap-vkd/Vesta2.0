ee3295542e194204f6ef76c5e425169ce23ce7f8
Kashyap <varunkashyap.vkd@gmail.com>
2017-01-05 16:14:45 +0530

    Image Popout Feature Added

---
+++ homepage/static/homepage/js/imagePopout.js
@@ -0,0 +1,75 @@
+var imagePopout = (function()
+{
+	var images, backgroundDiv, wrapperDiv, imageElement;
+
+	function createElements(){
+		backgroundDiv = document.createElement('div');
+		backgroundDiv.style.position = 'fixed';
+		backgroundDiv.style.width = '100vw';
+		backgroundDiv.style.height = '100vh';
+		backgroundDiv.style.backgroundColor = 'rgba(10, 10, 10, 0.4)';
+		backgroundDiv.style.top = '0px';
+		backgroundDiv.style.left = '0px';
+		backgroundDiv.style.zIndex = '5000';
+		backgroundDiv.style.display = 'none';
+
+		wrapperDiv = document.createElement('div');
+		wrapperDiv.style.position = 'fixed';
+		wrapperDiv.style.width = '40vw';
+		wrapperDiv.style.maxHeight = '80vh';
+		wrapperDiv.style.overflow = 'auto';
+		wrapperDiv.style.top = '10vh';
+		wrapperDiv.style.left = '30vw';
+		wrapperDiv.style.zIndex = '5001';
+		wrapperDiv.style.display = 'none';
+
+		newImage = document.createElement('img');
+		newImage.style.width = '100%';
+		newImage.style.height = 'auto';
+		imageElement = newImage;
+
+		wrapperDiv.appendChild(newImage);
+		document.body.appendChild(backgroundDiv);
+		document.body.appendChild(wrapperDiv);
+		backgroundDiv.addEventListener('click', hideElements);
+	}
+
+	function hideElements(event){
+		if(event.target == backgroundDiv){
+			backgroundDiv.style.display = 'none';
+			wrapperDiv.style.display = 'none';
+			document.body.style.overflow = 'auto';		
+		}
+	}
+
+	function displayLightBox(image){
+		backgroundDiv.style.display = 'block';
+		wrapperDiv.style.display = 'block';
+		document.body.style.overflow = 'hidden';
+		imageElement.setAttribute('src', image.getAttribute('src'));
+	}
+
+	function preventDefaults(event){
+		event.preventDefault();
+	}
+
+	function init(obj)
+	{
+		var anchors = document.getElementsByClassName(obj.anchors);
+		images = document.getElementsByClassName(obj.images);
+		createElements();
+
+		for(var i = 0; i < anchors.length; i++)
+			anchors[i].addEventListener('click', preventDefaults);
+		
+		for(var i = 0; i < images.length; i++)
+		{
+			images[i].style.cursor = 'pointer';
+			images[i].addEventListener('click', displayLightBox.bind(this, images[i]));
+		}
+	}
+
+	return {
+		'init' : init,
+	};
+})();
